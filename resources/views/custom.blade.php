4e35be6ccf8df0c3efb87edf5bfcca7364c59933
LwinMinKoKo <lwinminkk@gmail.com>
2023-01-19 11:47:50 +0630

    first day 1

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Custom Index</title>
+</head>
+<body>
+    <h1>Welcome from Custom page</h1>
+</body>
+</html>
