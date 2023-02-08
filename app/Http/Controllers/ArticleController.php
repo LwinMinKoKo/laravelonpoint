2e897b3f389fb067b3f5b6fc48a29ebc3c16e346
LwinMinKoKo <lwinminkk@gmail.com>
2023-02-08 23:45:22 +0630

    Data Extect form databse

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,22 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Article;
 
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
-    {
-        $data=[
-            ["id"=>1,"title"=>"First Article"],
-            ["id"=>2,"title"=>"Second Article"]
-        ];
-        return view('articles.index',['articles'=> $data]);    
-    }
+    // public function index()
+    // {
+    //     $data=[
+    //         ["id"=>1,"title"=>"First Article"],
+    //         ["id"=>2,"title"=>"Second Article"]
+    //     ];
+    //     return view('articles.index',['articles'=> $data]);    
+    // }
 
-    public function details($id)
-    {
-        return "Controller  - $id";
-    }
+    // public function details($id)
+    // {
+    //     return "Controller  - $id";
+    // }
+
+
+
+    // create new exercisecode
+        public function index()
+        {
+            $data=Article::all();
+            return view('articles.index',[
+                'articles'=>$data]);    
+        }
+
+        public function detail($id)
+        {
+            return " controller Article - $id";
+        }
 }
